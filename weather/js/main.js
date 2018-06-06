3a0b0895a13be36871ddc3d4799d87969e348559
LasyIsLazy <1255352506@qq.com>
2018-06-06 14:28:22 +0800

    Add transition.

--- weather/js/main.js
+++ weather/js/main.js
@@ -36,11 +36,23 @@ Vue.component('weather-item', {
     template: `
             <div class="weather-item">
                 <div>{{left}}</div>
-                <div class="weather-item-center"><slot></slot></div>
+                <div><slot></slot></div>
                 <div>{{right}}</div>
             </div>
             `
 });
+Vue.component('windmill', {
+    props: ['mystyle'],
+    template: `
+    <div class="windmill" :style="mystyle">
+    <div class="winmill-base"></div>
+    <div class="winmill-axis"></div>
+    <div class="winmill-blade rotate-0"></div>
+    <div class="winmill-blade rotate-120"></div>
+    <div class="winmill-blade rotate-240"></div>
+</div>
+    `
+})
 const app = new Vue({
     el: 'main',
     data: {
@@ -55,6 +67,9 @@ const app = new Vue({
         },
         weathers: [{}, {}, {}],
         acquired: false,
+        updating: false,
+        temperatureStatus: 'cool',
+        setting: false,
     },
     computed: {
         test: function () {
@@ -72,13 +87,15 @@ const app = new Vue({
         },
         updateWeather: function () {
             const city = this.selectedCity.name;
+            this.updating = true;
             return new Promise(resolve => {
                 getWeather(city, weather => {
                     resolve(weather);
                 });
             }).then((weather) => {
-                console.log(weather);
+                // console.log(weather);
                 this.weather = weather;
+                this.updateTemperatureStatus();
             }).then(() => {
                 return new Promise(resolve => {
                     getAllWeather(city, weathers => {
@@ -86,11 +103,27 @@ const app = new Vue({
                     });
                 });
             }).then((weathers) => {
-                console.log(weathers);
+                // console.log(weathers);
                 this.weathers = weathers;
             }).then(() => {
+                this.updating = false;
                 this.acquired = true;
             });
+        },
+        updateTemperatureStatus: function () {
+            const tem = this.weather.now.temperature;
+            this.temperatureStatus = (function () {
+                if (tem >= 30) {
+                    return 'hot';
+                } else if (tem >= 20) {
+                    return 'warm';
+                } else if (tem >= 10) {
+                    return 'cool';
+                } else {
+                    return 'cold';
+                }
+            })();
+
         }
     },
 });
